607d3e46004671fc8221e9d96c24c9a6e9171c39
emirez <erenmertirez@gmail.com>
2017-09-06 10:12:46 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#ifndef UNIT_TEST
+#ifdef ARDUINO
+
+#include <Arduino.h>
+
+/* need to include the module here */
+#include "mod1.h"
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+Serial.println("Hello World");
+delay(1000);
+}
+
+#else
+int main(int argc, char **argv) {
+}
+#endif
+#endif
